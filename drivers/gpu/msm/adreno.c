6a4cc78894913b8bfe0aa2dc1f53cbdc78ff389c
Harshdeep Dhatt <hdhatt@codeaurora.org>
2016-05-27 12:40:43 -0600

    msm: kgsl: Ignore EAGAIN when programming perfcounter
    When programming perfcounter via gpu commands, we may encounter
    -EAGAIN because of cancelling rb events either due to soft reset
    or when powering down the device. Ignore this error because we
    have already set up the perfcounter in software and it will be
    programmed in hardware by adreno_perfcounter_restore when gpu
    comes back up.
    
    CRs-Fixed: 1024199
    Change-Id: I5dc3561d15fa50ac58646f96559cfd262020dda9
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2259,8 +2259,6 @@ static int adreno_soft_reset(struct kgsl_device *device)
 			adreno_support_64bit(adreno_dev))
 		gpudev->enable_64bit(adreno_dev);
 
-	/* Restore physical performance counter values after soft reset */
-	adreno_perfcounter_restore(adreno_dev);
 
 	/* Reinitialize the GPU */
 	gpudev->start(adreno_dev);
@@ -2286,6 +2284,9 @@ static int adreno_soft_reset(struct kgsl_device *device)
 		device->reset_counter++;
 		set_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
 	}
+
+	/* Restore physical performance counter values after soft reset */
+	adreno_perfcounter_restore(adreno_dev);
 
 	return ret;
 }
