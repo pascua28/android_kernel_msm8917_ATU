55b8561edca0bb4735188d0d64760646503859f3
Andres Oportus <andresoportus@google.com>
2017-02-01 13:34:54 -0800

    ANDROID: cpufreq_stats: add per task/uid/freq stats
    Adds per process nodes in /proc/PID/time_in_state showing per
    frequency times and adds a global /proc/uid_time_in_state
    showing per frequency per uid times.
    
    Bug: 34133340
    Bug: 38320164
    
    Tests: boot sailfish and reading /proc/uid_time_in_state and
    /proc/$$/time_in_state
    
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: Ideb22b608b9a5e7bd2200a3a6df0f110b635f96a
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -76,6 +76,7 @@
 #include <linux/printk.h>
 #include <linux/cgroup.h>
 #include <linux/cpuset.h>
+#include <linux/cpufreq.h>
 #include <linux/audit.h>
 #include <linux/poll.h>
 #include <linux/nsproxy.h>
@@ -3052,6 +3053,9 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_HW_DIE_CATCH
 	REG("unexpected_die_catch", S_IRUGO|S_IWUSR, proc_unexpected_die_catch_operations),
 #endif
+#ifdef CONFIG_CPU_FREQ_STAT
+	ONE("time_in_state", 0444, proc_time_in_state_show),
+#endif
 };
 
 static int proc_tgid_base_readdir(struct file *file, struct dir_context *ctx)
@@ -3436,6 +3440,9 @@ static const struct pid_entry tid_base_stuff[] = {
 #endif
 #ifdef CONFIG_HW_VIP_THREAD
 	REG("static_vip", S_IRUGO, proc_static_vip_operations),
+#endif
+#ifdef CONFIG_CPU_FREQ_STAT
+	ONE("time_in_state", 0444, proc_time_in_state_show),
 #endif
 };
 
