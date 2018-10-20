4cc25e3b13f8292a58f7f09283be8ebe00ead723
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-10-20 13:19:04 +0700

    drivers: thermal: Use deferrable work and power efficient workqueue
    Thermal core uses work events to poll for sensor driver temperature
    crossing a threshold. Since it is not using a deferrable workqueue, it
    might wake-up the device from sleep.
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3032,7 +3032,8 @@ static int __ref update_offline_cores(int val)
 
 	if (pend_hotplug_req && !in_suspend && !retry_in_progress) {
 		retry_in_progress = true;
-		schedule_delayed_work(&retry_hotplug_work,
+		mod_delayed_work(system_freezable_power_efficient_wq,
+			&retry_hotplug_work,
 			msecs_to_jiffies(HOTPLUG_RETRY_INTERVAL_MS));
 	}
 
@@ -3525,7 +3526,8 @@ static void check_temp(struct work_struct *work)
 
 reschedule:
 	if (polling_enabled)
-		schedule_delayed_work(&check_temp_work,
+		mod_delayed_work(system_freezable_power_efficient_wq,
+				&check_temp_work,
 				msecs_to_jiffies(msm_thermal_info.poll_ms));
 }
 
@@ -5265,9 +5267,10 @@ int msm_thermal_init(struct msm_thermal_data *pdata)
 
 	register_reboot_notifier(&msm_thermal_reboot_notifier);
 	pm_notifier(msm_thermal_suspend_callback, 0);
-	INIT_DELAYED_WORK(&retry_hotplug_work, retry_hotplug);
-	INIT_DELAYED_WORK(&check_temp_work, check_temp);
-	schedule_delayed_work(&check_temp_work, 0);
+	INIT_DEFERRABLE_WORK(&retry_hotplug_work, retry_hotplug);
+	INIT_DEFERRABLE_WORK(&check_temp_work, check_temp);
+	mod_delayed_work(system_freezable_power_efficient_wq,
+		&check_temp_work, 0);
 
 	if (num_possible_cpus() > 1) {
 		cpus_previously_online_update();
