45e0319a383b00530f3b035c327f3fd69a89b3c3
Samuel Pascua <pascua.samuel.14@gmail.com>
2018-11-26 13:55:41 +0800

    import msm hotplug

--- arch/arm64/kernel/alucard_hotplug.c
+++ arch/arm64/kernel/alucard_hotplug.c
@@ -68,7 +68,7 @@ static struct hotplug_tuners {
 #endif
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 65,
-	.hotplug_enable = 1,
+	.hotplug_enable = 0,
 	.min_cpus_online = 1,
 	.maxcoreslimit = 4,
 	.hp_io_is_busy = 1,
