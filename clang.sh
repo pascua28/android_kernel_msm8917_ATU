2b9d77be8b72cc40f0089a20b81d42a731a96955
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-01-24 19:07:02 +0800

    add clang build script

---
+++ clang.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd $(readlink -f .)
+
+if [ ! -f .config ]; then
+	cp arch/arm64/configs/atu_defconfig .config
+	make -j5 ARCH=arm64 oldconfig
+else
+	make -j5 ARCH=arm64 oldconfig
+fi
+
+export KBUILD_COMPILER_STRING=$(~/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+
+DATE_START=$(date +"%s")
+make -j$(nproc --all) \
+ARCH=arm64 \
+CC=~/clang/bin/clang \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=~/linaro/bin/aarch64-linux-gnu- \
+Image.gz-dtb
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
