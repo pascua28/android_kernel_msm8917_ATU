e723fe0fb54c9cb762961ce447d83f0f166e1323
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-02-03 18:51:03 +0800

    ANDROID: mmc: move to a SCHED_FIFO thread

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -21,6 +21,7 @@
 
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
+#include <linux/sched/rt.h>
 #include "queue.h"
 #ifdef CONFIG_HUAWEI_EMMC_DSM
 #include <linux/mmc/dsm_emmc.h>
@@ -91,6 +92,11 @@ static inline void mmc_cmdq_ready_wait(struct mmc_host *host,
 {
 	struct mmc_cmdq_context_info *ctx = &host->cmdq_ctx;
 	struct request_queue *q = mq->queue;
+	struct sched_param scheduler_params = {0};
+
+	scheduler_params.sched_priority = 1;
+
+	sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
 
 	/*
 	 * Wait until all of the following conditions are true:
