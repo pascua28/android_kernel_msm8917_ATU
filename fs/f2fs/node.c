cdd6cd6023d32fa071c8c89f532331deb462e54f
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-02-06 11:10:51 +0800

    f2fs: sync some changes from Linux 4.13-rc1

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -2669,7 +2669,7 @@ static int init_free_nid_cache(struct f2fs_sb_info *sbi)
 {
 	struct f2fs_nm_info *nm_i = NM_I(sbi);
 
-	nm_i->free_nid_bitmap = f2fs_vzalloc(nm_i->nat_blocks *
+	nm_i->free_nid_bitmap = kvzalloc(nm_i->nat_blocks *
 					NAT_ENTRY_BITMAP_SIZE, GFP_KERNEL);
 	if (!nm_i->free_nid_bitmap)
 		return -ENOMEM;
