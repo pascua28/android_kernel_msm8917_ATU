9303a452d22c854bc798ec674f200f5bfa235e16
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-12-15 17:40:06 +0800

    Revert "build_script: use sdclang"
    This reverts commit 2861d7ebf4598d3510774089bde5ef9d395ce904.

--- clang.sh
+++ clang.sh
@@ -8,12 +8,12 @@ else
 	make -j5 ARCH=arm64 oldconfig
 fi
 
-export KBUILD_COMPILER_STRING=$(~/sdclang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export KBUILD_COMPILER_STRING=$(~/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 DATE_START=$(date +"%s")
 make -j$(nproc --all) \
 ARCH=arm64 \
-CC=~/sdclang/bin/clang \
+CC=~/clang/bin/clang \
 CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=~/linaro/bin/aarch64-linux-gnu- \
 Image.gz-dtb
